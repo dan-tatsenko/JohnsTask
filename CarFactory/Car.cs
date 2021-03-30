c590d8d6d9379ea4e11175e7131aece63f5ea271
Danila Tatsenko AGILITY <danil.tatsenko@hotmail.com>
2021-03-31 09:36:32 +1300

    Created class Car

---
+++ CarFactory/Car.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CarFactory
+{
+    public class Car
+    {
+        public  int Id { get; set; }
+        public string Make { get; set; }
+        public  string  Model { get; set; }
+        public string Colour { get; set; }
+    }
+}
